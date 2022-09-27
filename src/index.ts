734f6bb0881273da0a7d986c7a74c338c43b230e
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-09-27 18:38:55 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -50,10 +50,10 @@ prisma.$connect().then(() => console.log(chalk.bgBlack('connected to database'))
     if (typeof commands.get(command) !== 'undefined') commands?.get(command)?.default.execute(message, config, args);
   });
 
-  // remove later
-  console.log(process.env.BOT_TOKEN);
-  console.log(process.env.DATABASE_URL);
   client.login(process.env.BOT_TOKEN);
 })()
-  .catch(err => console.log(err))
+  .catch(err => {
+    console.log('error');
+    console.error(err);
+  })
   .finally(async () => await prisma.$disconnect());
