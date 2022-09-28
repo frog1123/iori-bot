5d017a6c4125d7a6bae2be9f65b8bf046d06ceeb
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-09-27 20:34:55 -0500

    test

--- src/index.ts
+++ src/index.ts
@@ -19,40 +19,40 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 export const prisma = new PrismaClient();
 prisma.$connect().then(() => console.log(chalk.bgBlack('connected to database')));
 
-(async () => {
-  // set commands
-  const commands = new Collection() as Collection<string, Command>;
-  const commandFiles = readdirSync(join(__dirname, 'dist', 'commands')).filter(file => file.endsWith('.js'));
+// (async () => {
+//   // set commands
+//   const commands = new Collection() as Collection<string, Command>;
+//   const commandFiles = readdirSync(join(__dirname, 'dist', 'commands')).filter(file => file.endsWith('.js'));
 
-  for (const file of commandFiles) {
-    const command = (await import(`./commands/${file}`)) as unknown as Command;
-    commands.set(command.default.name, command);
-  }
-  console.log(chalk.bgBlack(`loaded commands from ${commandFiles}`));
+//   for (const file of commandFiles) {
+//     const command = (await import(`./commands/${file}`)) as unknown as Command;
+//     commands.set(command.default.name, command);
+//   }
+//   console.log(chalk.bgBlack(`loaded commands from ${commandFiles}`));
 
-  // start
-  client.on('ready', () => {
-    console.log(chalk.bgBlack(`logged in as ${client?.user?.tag}`));
-    client?.user?.setPresence({ status: 'idle' });
-    client?.user?.setActivity(`${client.guilds.cache.size} server${client.guilds.cache.size > 1 ? 's' : ''}`, { type: ActivityType.Watching });
-  });
+//   // start
+//   client.on('ready', () => {
+//     console.log(chalk.bgBlack(`logged in as ${client?.user?.tag}`));
+//     client?.user?.setPresence({ status: 'idle' });
+//     client?.user?.setActivity(`${client.guilds.cache.size} server${client.guilds.cache.size > 1 ? 's' : ''}`, { type: ActivityType.Watching });
+//   });
 
-  // refresh server watch count
-  setInterval(() => client?.user?.setActivity(`${client.guilds.cache.size} server${client.guilds.cache.size > 1 ? 's' : ''}`, { type: ActivityType.Watching }), 300000);
-  client.on('messageCreate', message => {
-    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+//   // refresh server watch count
+//   setInterval(() => client?.user?.setActivity(`${client.guilds.cache.size} server${client.guilds.cache.size > 1 ? 's' : ''}`, { type: ActivityType.Watching }), 300000);
+//   client.on('messageCreate', message => {
+//     if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-    const args = message.content.slice(config.prefix.length).split(/ + /).toString().split(' ');
-    const command = args!.shift()!.toLowerCase().split(' ')[0];
+//     const args = message.content.slice(config.prefix.length).split(/ + /).toString().split(' ');
+//     const command = args!.shift()!.toLowerCase().split(' ')[0];
 
-    // execute command if it exists
-    if (typeof commands.get(command) !== 'undefined') commands?.get(command)?.default.execute(message, config, args);
-  });
+//     // execute command if it exists
+//     if (typeof commands.get(command) !== 'undefined') commands?.get(command)?.default.execute(message, config, args);
+//   });
 
-  client.login(process.env.BOT_TOKEN);
-})()
-  .catch(err => {
-    console.log('error');
-    console.error(err);
-  })
-  .finally(async () => await prisma.$disconnect());
+//   client.login(process.env.BOT_TOKEN);
+// })()
+//   .catch(err => {
+//     console.log('error');
+//     console.error(err);
+//   })
+//   .finally(async () => await prisma.$disconnect());
